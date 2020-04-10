38244fca5c9b798647e0c785e1b877f4e98f4314
kenjinp <github@kenny.wtf>
2020-04-10 22:23:28 +0200

    graphs from model output

--- app/API.ts
+++ app/API.ts
@@ -1,6 +1,68 @@
 import { apiBase } from './config';
 import csv from 'csvtojson';
 
+export type SimulationParams = {
+  total_pop: number;
+  pop_hospitals: number;
+  pop_high_contact: number;
+  prop_urban: number;
+  prop_isolated: number;
+  degraded: number;
+  ge_65: number;
+  prop_tests_hospitals: number;
+  prop_tests_high_contact: number;
+  prop_tests_rest_of_population: number;
+  sensitivity_PCR: number;
+  sensitivity_RDT: number;
+  sensitivity_xray: number;
+  selectivity_PCR: number;
+  selectivity_RDT: number;
+  selectivity_xray: number;
+  num_tests_PCR: number;
+  num_tests_RDT: number;
+  num_tests_xray: number;
+};
+
+export type SimulationResponse = {
+  scenarios: {
+    maxInfected: number;
+    maxIsolated: number;
+    totalDeaths: number;
+    totalTests: number;
+    data: string;
+  }[];
+};
+
+export type Scenarios = {
+  maxInfected: number;
+  maxIsolated: number;
+  totalDeaths: number;
+  totalTests: number;
+  data: {
+    beta: string;
+    compartment: string;
+    days: string;
+    field1: string;
+    new_tested: string;
+    num_confirmed: string;
+    num_deaths: string;
+    num_infected: string;
+    num_isolated: string;
+    num_isolated_infected: string;
+    num_recovered: string;
+    population: string;
+    susceptible_prop: string;
+    susceptibles: string;
+    tested: string;
+    total_confirmed: string;
+    total_deaths: string;
+    total_infected: string;
+    total_infected_notisolated: string;
+    total_isolated: string;
+    total_recovered: string;
+  }[];
+}[];
+
 export default class API {
   base: string;
   constructor() {
@@ -24,9 +86,26 @@ export default class API {
     );
   }
 
-  simulation() {
-    return fetch(`${this.base}/simulation`, { method: 'POST' })
-      .then(response => response.text())
-      .then(string => csv().fromString(string));
+  async simulation(simulationParams: SimulationParams): Promise<Scenarios> {
+    const formData = new FormData();
+    Object.keys(simulationParams).forEach(key => {
+      formData.append(key, simulationParams[key]);
+    });
+    const response = await fetch(`${this.base}/simulation`, {
+      method: 'POST',
+      body: formData,
+    });
+    const simulationResponse: SimulationResponse = await response.json();
+    const csvData = await Promise.all(
+      simulationResponse.scenarios.map(scenario => {
+        return csv().fromString(scenario.data);
+      }),
+    );
+    return simulationResponse.scenarios.map((scenario, index) => {
+      return {
+        ...scenario,
+        data: csvData[index],
+      };
+    });
   }
 }
---
+++ app/App.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import TopSection from './components/TopSection';
+import TestSelector from './components/TestSelector';
+import Countries from './containers/countries';
+import Simulation from './containers/simulation';
+import About from './components/About';
+
+const simulationParams = {
+  total_pop: 80000000,
+  pop_hospitals: 10000,
+  pop_high_contact: 1000000,
+  prop_urban: 0.46,
+  prop_isolated: 0.1,
+  degraded: 0.1,
+  ge_65: 0.25,
+  prop_tests_hospitals: 0.5,
+  prop_tests_high_contact: 0.5,
+  prop_tests_rest_of_population: 0,
+  sensitivity_PCR: 0.95,
+  sensitivity_RDT: 0.85,
+  sensitivity_xray: 0.9,
+  selectivity_PCR: 0.95,
+  selectivity_RDT: 0.9,
+  selectivity_xray: 0.9,
+  num_tests_PCR: 1000,
+  num_tests_RDT: 1000,
+  num_tests_xray: 1000,
+};
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <main>
+        <TopSection />
+        <Countries />
+        <TestSelector />
+        <Simulation simulationParams={simulationParams} />
+        <About />
+      </main>
+    </div>
+  );
+};
+
+export default App;
