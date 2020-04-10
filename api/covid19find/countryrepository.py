6babd19c672eb4e050a9e5fa2ed834e88c8212d2
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2020-04-10 11:40:00 +0200

    Fix over 65 data for country.

--- api/covid19find/countryrepository.py
+++ api/covid19find/countryrepository.py
@@ -22,16 +22,21 @@ class CountryRepository:
         # (population, urban_population_percentage, urban_population_in_degraded_housing_percentage, over_65_percentage,
         #  hospital_employment, high_contact_population, remote_areas_population_percentage) =
         pcountry = pycountry.countries.get(alpha_2=country_code)
+
         if pcountry is None:
             return None
         country = Country(int(pcountry.numeric), 2020, age=65)
+        over65percentage = None
+        over65count = country.get_overX()
+        if over65count is not None:
+            over65percentage = over65count / country.get_population()
 
         return {
             "countryCode": country_code,
             "population": country.get_population(),
             "urbanPopulationPercentage": country.get_pcnt_urban(),
             "urbanPopulationInDegradedHousingPercentage": country.get_pcnt_degraded(),
-            "over65Percentage": country.get_overX(),
+            "over65Percentage": over65percentage,
             "hospitalEmployment": None,
             "hospitalBeds": country.get_hosp_beds(),
             "highContactPopulation": country.get_high_contact(),
