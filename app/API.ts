30633f1cb10fa97454a2ed925330549d3740cb1b
kenjinp <github@kenny.wtf>
2020-04-28 09:28:40 +0200

    change defaults

--- app/API.ts
+++ app/API.ts
@@ -42,7 +42,7 @@ export const DEFAULT_SCENARIO_LIST: Scenario[] = [
     interventionType: InterventionType.LOCKDOWN,
     description:
       'In this scenario, all available tests are used to test hospital staff, if possible repeatedly.  Tests are limited to individuals already showing symptoms. The goal is to reduce the burden of the epidemic on hospital staff, preserving the capabilities necessary to help others.',
-    interventionTiming: InterventionTiming.GT500,
+    interventionTiming: InterventionTiming.GT50,
     testSymptomaticOnly: true,
     hospitalTestProportion: 100,
     otherHighContactPopulationTestProportion: 0,
