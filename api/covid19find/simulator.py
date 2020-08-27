d77062888d6641fea33aac1eb6d5fcc7b602ecdd
Wojciech Wajerowicz <wojciech.wajerowicz@epfl.ch>
2020-08-27 15:23:26 +0200

    Return simulation data in a new format.

--- api/covid19find/simulator.py
+++ api/covid19find/simulator.py
@@ -1,22 +1,26 @@
-from .simulation.covidlib import run_simulation, getscenarios
+from .simulation.covidlib import run_simulation, get_scenarios
+import numpy as np
+import pandas as pd
 
 
 class Simulator:
-    TIMING_PARAMS = {
-        "very_early": 0,
-        "early": 1,
-        "late": 2,
-        "very_late": 3,
-        "never": 4
-    }
-    INTERVENTION_PARAMS = {
-        "no_intervention": 0,
-        "mild_intervention": 1,
-        "lockdown": 2
-    }
+    SIMULATION_DAYS = 186
 
-    REVERSE_TIMING_PARAMS = {v: k for k, v in TIMING_PARAMS.items()}
-    REVERSE_INTERVENTION_PARAMS = {v: k for k, v in INTERVENTION_PARAMS.items()}
+    def __init__(self, covid_repository):
+        self.covid_repository = covid_repository
+
+    @staticmethod
+    def __map_datapoint(dp):
+        return {
+            "Date": dp["date"],
+            "accumulated_deaths": dp["totalDeaths"],
+            "accumulated_cases": dp["totalConfirmed"],
+            "tests": dp["newTests"]
+        }
+
+    def get_country_df(self, country_code):
+        covid_data = self.covid_repository.data_for(country_code)["timeseries"][:self.SIMULATION_DAYS]
+        return pd.DataFrame.from_records(list(map(self.__map_datapoint, covid_data)))
 
     @staticmethod
     def get_fixed_parameters(parameters):
@@ -28,61 +32,116 @@ class Simulator:
             "prop_urban": parameters["urbanPopulationProportion"],
             "prop_below_pl": parameters["belowPovertyLineProportion"],
             "prop_woh": parameters["workingOutsideHomeProportion"],
-            "staff_per_bed": parameters["hospitalStaffPerBed"],
-            "sensitivity_PCR": parameters["sensitivityPCR"],
-            "sensitivity_RDT": parameters["sensitivityRDT"],
-            "sensitivity_xray": parameters["sensitivityXray"],
-            "specificity_PCR": parameters["specificityPCR"],
-            "specificity_RDT": parameters["specificityRDT"],
-            "specificity_xray": parameters["specificityXray"],
-            "num_tests_PCR": parameters["numTestsPCR"],
-            "num_tests_RDT": parameters["numTestsRDT"],
-            "num_tests_xray": parameters["numTestsXray"]
+            "staff_per_bed": parameters["hospitalStaffPerBed"]
         }
 
     @staticmethod
     def get_scenario_parameters(parameters):
-        if "scenarios" in parameters:
-            return list(map(Simulator.__map_scenario, parameters["scenarios"]))
-        else:
-            return None
+        return list(map(Simulator.__map_scenario, parameters["scenarios"]))
 
     @staticmethod
     def __map_scenario(input_scenario):
+        phases = input_scenario["phases"]
         return {
-            "intervention_type": Simulator.INTERVENTION_PARAMS[input_scenario["interventionType"]],
-            "intervention_timing": Simulator.TIMING_PARAMS[input_scenario["interventionTiming"]],
-            "symptomatic_only": str(input_scenario["testSymptomaticOnly"]),
-            "prop_hospital": input_scenario["hospitalTestProportion"],
-            "prop_other_hc": input_scenario["otherHighContactPopulationTestProportion"],
-            "prop_rop": 1 - input_scenario["hospitalTestProportion"] - input_scenario[
-                "otherHighContactPopulationTestProportion"]
+            "imported_infections_per_day": Simulator.__get_array_for_key(phases, "importedInfectionsPerDay"),
+            "severity": Simulator.__get_array_for_key(phases, "severity"),
+            "trig_values": Simulator.__get_array_for_key(phases, "trigger"),
+            "trig_def_type": Simulator.__get_array_for_key(phases, "triggerType"),
+            "trig_op_type": Simulator.__get_array_for_key(phases, "triggerCondition"),
+            "prop_contacts_traced": Simulator.__get_array_for_key(phases, "proportionOfContactsTraced"),
+            "num_tests_mitigation": Simulator.__get_array_for_key(phases, "numTestsMitigation"),
+            "type_tests_mitigation": Simulator.__get_array_for_key(phases, "typeTestsMitigation"),
+            "sensitivity": Simulator.__get_array_for_key(phases, "sensitivity"),
+            "specificity": Simulator.__get_array_for_key(phases, "specificity"),
+            "symptomatic_only": Simulator.__get_array_for_key(phases, "testSymptomaticOnly", str),
+            "confirmation_tests": Simulator.__get_array_for_key(phases, "confirmationTests", str),
+            "prop_hospital": Simulator.__get_array_for_key(phases, "hospitalTestProportion"),
+            "prop_other_hc": Simulator.__get_array_for_key(phases, "otherHighContactPopulationTestProportion"),
+            "prop_rop": Simulator.__get_array_for_key(phases, "restOfPopulationTestProportion"),
+            "test_multipliers": [0, 1, 2, 3],
+            "num_tests_care": Simulator.__get_array_for_key(phases, "numTestsCare"),
+            "type_tests_care": Simulator.__get_array_for_key(phases, "typeTestsCare"),
+            "requireddxtests": Simulator.__get_array_for_key(phases, "requiredDxTests")
         }
+
+    @staticmethod
+    def __get_array_for_key(scenarios, key, func=lambda x: x):
+        return list(map(lambda scenario: func(scenario[key]), scenarios))
 
     def run(self, parameters):
         scenarios = self.get_scenario_parameters(parameters)
-        if scenarios is not None:
-            result = run_simulation(self.get_fixed_parameters(parameters), scenarios=scenarios)
-        else:
-            result = run_simulation(self.get_fixed_parameters(parameters))
+        country_df = self.get_country_df(parameters["countryCode"])
+        print(country_df.iloc[55])
+        result = run_simulation(country_df, self.get_fixed_parameters(parameters), scenarios=scenarios)
 
         scenario_data = []
         scenario_dfs = result[0]
-        scenario_total_tests = result[1]
-        scenario_total_deaths = result[2]
-        scenario_max_infected = result[3]
-        scenario_max_isolated = result[4]
-        for i in range(0, len(scenario_dfs)):
+        scenario_totals = result[2]
+        for i in range(0, len(scenario_totals["total_deaths_by_scenario"])):
+            scenarios_compartments_df = scenario_dfs[i * 2]
             scenario_data.append(
                 {
-                    "data": scenario_dfs[i].to_csv(),
-                    "totalTests": scenario_total_tests[i],
-                    "totalDeaths": scenario_total_deaths[i],
-                    "maxInfected": scenario_max_infected[i],
-                    "maxIsolated": scenario_max_isolated[i]
+                    "totalTests": int(scenario_totals["total_tests_mit_by_scenario"][i]),
+                    "totalDeaths": int(scenario_totals["total_deaths_by_scenario"][i]),
+                    "maxInfected": int(scenario_totals["max_infected_by_scenario"][i]),
+                    "totalInfected": int(scenario_totals["total_infected_by_scenario"][i]),
+                    "maxIsolated": int(scenario_totals["max_isolated_by_scenario"][i]),
+                    "data": {
+                        "hospitals": self.__dataframe_to_response(
+                            scenarios_compartments_df[scenarios_compartments_df.compartment.eq("Hospitals")]),
+                        "otherHighContact": self.__dataframe_to_response(
+                            scenarios_compartments_df[scenarios_compartments_df.compartment.eq("Other high contact ")]),
+                        "restOfPopulation": self.__dataframe_to_response(
+                            scenarios_compartments_df[scenarios_compartments_df.compartment.eq("Rest of population")]),
+                        "total": self.__dataframe_to_response(scenario_dfs[i * 2 + 1])
+                    }
                 }
             )
-        return scenario_data
+        return {"scenarios": scenario_data}
+
+    @staticmethod
+    def __dataframe_to_response(df):
+        return list(map(Simulator.__dataframe_row_to_response, df.iterrows()))
+
+    @staticmethod
+    def __dataframe_row_to_response(index_row):
+        row = index_row[1]
+        return {
+            "day": int(row["days"]),
+            "date": row["dates"].date().isoformat(),
+            "population": int(row["population"]),
+            "susceptibles": int(row["susceptibles"]),
+            "currentIsolated": int(row["isolated"]),
+            "currentInfected": int(row["infected"]),
+            "totalConfirmed": int(row["confirmed"]),
+            "totalInfected": int(row["accumulatedinfected"]),
+            "totalTested": int(row["tested_mit"]),
+            "currentInfectedNotIsolated": int(row["infectednotisolated"]),
+            "totalDeaths": int(row["deaths"]),
+            "totalRecovered": int(row["recovered"]),
+            "beta": row["beta"],
+            "rEff": row.get("reff", None),
+            "susceptibleProportion": row["susceptibleprop"],
+            "newTests": int(row["newtested_mit"]),
+            "newInfected": int(row["newinfected"]),
+            "newConfirmed": int(row["newconfirmed"]),
+            "newIsolated": int(row["newisolated"]),
+            "newIsolatedInfected": int(row["newisolatedinfected"]),
+            "newRecovered": int(row["newrecovered"]),
+            "requiredDxTests": int(row["requireddxtests"]),
+            "actualDxTest": int(row["actualdxtests"]),
+            "newDeaths": int(row["newdeaths"]),
+            "truePositives": int(row["truepositives"]),
+            "falsePositives": int(row["falsepositives"]),
+            "trueNegatives": int(row["truenegatives"]),
+            "falseNegatives": int(row["falsenegatives"]),
+            "ppv": row["ppv"],
+            "npv": row["npv"],
+            "incidence": row["incidence"],
+            "prevalence": row["prevalence"],
+            "newTestsPositiveProportion": row.get("positive_rate", None),
+            "detectionRate": row.get("detection_rate", None)
+        }
 
     @staticmethod
     def __reverse_map_scenario(covidlib_scenario):
