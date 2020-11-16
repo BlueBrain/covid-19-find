7ea4d842e4cedae0dbfcae300691b00aa095b14d
Kenneth Pirman <github@kenny.wtf>
2020-11-16 10:10:27 +0100

    expand scenario limit to 4

--- app/config.ts
+++ app/config.ts
@@ -5,7 +5,7 @@ export const apiBase =
   process.env.NODE_ENV === 'production' ? apiURL : 'http://localhost:5000/api';
 
 // How many scenarios are supported in the UI
-export const MAX_SCENARIOS_COUNT = 3;
+export const MAX_SCENARIOS_COUNT = 4;
 
 // How many phases are supported per scenario
 export const MAX_PHASE_COUNT = 3;
