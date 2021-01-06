4f9f45a19dcb1a3d3094305353f2a63cd4ce62bc
Kenneth Pirman <github@kenny.wtf>
2021-01-06 15:53:02 +0100

    remove unecessary prop

--- app/components/ScenarioEditor/PhaseNumberInput.tsx
+++ app/components/ScenarioEditor/PhaseNumberInput.tsx
@@ -32,7 +32,6 @@ const PhaseNumberInput: React.FC<{
       min={inputProps.min}
       max={inputProps.max}
       step={inputProps.step}
-      defaultValue={defaultValue}
       value={inputValue}
       type="number"
       required
