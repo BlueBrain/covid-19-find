320894a69d936c6c69e0bca82a63442afebe41e4
Kenneth Pirman <github@kenny.wtf>
2021-01-07 10:51:34 +0100

    disable save button when forms are invalid or dirty

--- app/components/SaveScenariosButton/index.tsx
+++ app/components/SaveScenariosButton/index.tsx
@@ -4,14 +4,15 @@ import { save } from '../../libs/stateLoader';
 
 const SaveScenariosButton: React.FC<{
   state: ClientSimulationRequest;
-}> = ({ state }) => {
+  disabled: boolean;
+}> = ({ state, disabled }) => {
   const handleSave = () => {
     save(state);
   };
 
   return (
     <div>
-      <button onClick={handleSave}>Save Scenarios</button>
+      <button disabled={disabled} onClick={handleSave}>Save Scenarios</button>
     </div>
   );
 };
