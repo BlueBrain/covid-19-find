2c924f52ec84aa21d6ef1b7489cc018f16956228
Kenneth Pirman <github@kenny.wtf>
2021-02-15 11:46:10 +0100

    fix rEff chart scale

--- app/components/SimulationResults/Graphs/RNaught.tsx
+++ app/components/SimulationResults/Graphs/RNaught.tsx
@@ -63,7 +63,7 @@ const RNaught: React.FC<{
                   color: '#00000005',
                 },
                 ticks: {
-                  max: 4,
+                  // max: 4,
                   beginAtZero: true,
                   callback: function(value, index, values) {
                     return value?.toLocaleString(undefined, {
