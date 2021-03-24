46e29868c8abe9a750d0f585344b564aa9e25aaf
rwalker1501 <rwalker1501@gmail.com>
2021-03-24 10:39:47 +0100

    New db1 and associated covidlib

--- api/covid19find/simulation/fitdeaths.py
+++ api/covid19find/simulation/fitdeaths.py
@@ -3,7 +3,7 @@ import optlib as opt
 import sys
 import cdata as cd
 
-ccode = 'PH'
+ccode = 'DE'
 if len(sys.argv) > 1:
    ccode = sys.argv[1]
    
@@ -12,7 +12,7 @@ if len(sys.argv) > 1:
 cname = cd.getcountryname(ccode)
 score,dfx,sev,trig,longsev,longtrig = opt.computephases(ccode)
 print("RESULT:",cname,",",sev,",",trig,",",score)
-opt.showthiscase(dfx,sev,trig,'TESTRW19')
+opt.showthiscase(dfx,sev,trig,'TESTRW22')
 
 
 
