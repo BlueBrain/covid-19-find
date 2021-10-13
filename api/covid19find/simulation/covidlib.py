344aa41da4d4c8f9179792b7ddcf5de6ae97203d
rwalker1501 <rwalker1501@gmail.com>
2021-10-13 22:08:53 +0200

    Corrected an infinities problem with ppv and npv. Corrected possible out of bounds for tp, fp, tn,fn

--- api/covid19find/simulation/covidlib.py
+++ api/covid19find/simulation/covidlib.py
@@ -482,8 +482,10 @@ def process_scenarios(country_df,p,scenarios,initial_beta, params_dir,end_date):
       dfsum['detection rate'].where(dfsum['detection rate']<=1, other=0,inplace=True)
       dfsum['ppv']=dfsum['truepositives']/(dfsum['truepositives']+dfsum['falsepositives'])
       dfsum['ppv'].where(dfsum['ppv']<=1, other=0,inplace=True)
+      dfsum['ppv'].where(dfsum['ppv']<0, other=0,inplace=True)
       dfsum['npv']=dfsum['truenegatives']/(dfsum['truenegatives']+dfsum['falsenegatives'])
-      dfsum['ppv'].where(dfsum['npv']<=1, other=0,inplace=True)
+      dfsum['npv'].where(dfsum['npv']<=1, other=0,inplace=True)
+      dfsum['npv'].where(dfsum['npv']<0, other=0,inplace=True)
       dfsum['incidence']=dfsum['newinfected']/(dfsum['population'])
       dfsum['incidence'].where(dfsum['incidence']>0, other=0,inplace=True)
       dfsum['prevalence']=(dfsum['accumulatedinfected']-dfsum['deaths'])/dfsum['population']
@@ -1756,9 +1758,7 @@ def adjust_positives_and_negatives(sim,par,t,phase,tests_performed,p_infected):
 # =============================================================================
         if t<today:
            sim.truenegatives[t,i]=tests_performed[i]-cases[i]
-           if sim.truenegatives[t,i]<0:
-              sim.truenegatives[t,i]=0
-           sim.falsepositives[t,i]= sim.truenegatives[t,i]*(1-par.specificity[phase])
+           sim.falsepositives[t,i]= sim.truenegatives[t,i]*(1-par.specificity[phase]) 
            sim.truepositives[t,i]=cases[i]-sim.falsepositives[t,i]
            sim.falsenegatives[t,i]=sim.truepositives[t,i]*(1-par.sensitivity[phase])
         else:
@@ -1766,7 +1766,14 @@ def adjust_positives_and_negatives(sim,par,t,phase,tests_performed,p_infected):
            sim.truenegatives[t,i]=tests_performed[i]*(1-p_infected[i])*par.specificity[phase]
            sim.falsepositives[t,i]=sim.truenegatives[t,i]*(1-par.specificity[phase])   
            sim.falsenegatives[t,i]=tests_performed[i]*p_infected[i]*(1-par.sensitivity[phase])
-    
+        if sim.truenegatives[t,i]<0:
+              sim.truenegatives[t,i]=0
+        if sim.falsepositives[t,i]<0:
+              sim.falsepositives[t,i]=0
+        if sim.truepositives[t,i]<0:
+              sim.truepositives[t,i]=0
+        if sim.falsenegatives[t,i]<0:
+              sim.falsenegatives[t,i]=0
            
 # =============================================================================
 #    #    sim.truepositives[t,i] = testsperformed[i] * p_infected[i]
